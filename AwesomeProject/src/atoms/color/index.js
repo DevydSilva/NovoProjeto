a8a01e455a13e06a0c58d5b1b90b18f69b69d60c
Devyd Silva <devyd.santana20201@gmail.com>
2023-01-22 13:01:56 -0300

    coll

--- AwesomeProject/src/atoms/color/index.js
+++ AwesomeProject/src/atoms/color/index.js
@@ -1,10 +1,9 @@
-export const azul = "#63C2D1"
-export const Azul2 = "#268596"
-export const azul3 = "#83D6E3"
-export const Azul4 = " #4EADBE"
-export const Banco = "#FFFFFF"
-export const Preto = "#000000"
-export const Preto2 ="rgba(0, 0, 0, 0.5)"
-export const CinzaClaro ="#737373"
-export const CinzaClaro2 ="#999999"
-
+export const azul = '#63C2D1';
+export const Azul2 = '#268596';
+export const azul3 = '#83D6E3';
+export const Azul4 = ' #4EADBE';
+export const Banco = '#FFFFFF';
+export const Preto = '#000000';
+export const Preto2 = 'rgba(0, 0, 0, 0.5)';
+export const CinzaClaro = '#737373';
+export const CinzaClaro2 = '#999999';
